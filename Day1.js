5a843be71d037b7d13f0c0f326566305c8230d55
Veronica <veronica_baldin@live.it>
2022-12-01 16:41:12 +0100

    Day 1

---
+++ Day1.js
@@ -0,0 +1,67 @@
+
+import { AocClient } from 'advent-of-code-client';
+import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config();
+import express from 'express';
+
+const client = new AocClient({
+  year: 2022, // the year of the challenge
+  day: 1, // the day of the challenge
+  token: process.env.TOKEN // the session cookie from adventofcode.com
+});
+
+const input = await client.getInput();
+
+/* The jungle must be too overgrown and difficult to navigate in vehicles or access from the air; the Elves' expedition traditionally goes on foot. As your boats approach land, the Elves begin taking inventory of their supplies. One important consideration is food - in particular, the number of Calories each Elf is carrying (your puzzle input).
+
+The Elves take turns writing down the number of Calories contained by the various meals, snacks, rations, etc. that they've brought with them, one item per line. Each Elf separates their own inventory from the previous Elf's inventory (if any) by a blank line.
+
+For example, suppose the Elves finish writing their items' Calories and end up with the following list:
+
+1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000
+This list represents the Calories of the food carried by five Elves:
+
+The first Elf is carrying food with 1000, 2000, and 3000 Calories, a total of 6000 Calories.
+The second Elf is carrying one food item with 4000 Calories.
+The third Elf is carrying food with 5000 and 6000 Calories, a total of 11000 Calories.
+The fourth Elf is carrying food with 7000, 8000, and 9000 Calories, a total of 24000 Calories.
+The fifth Elf is carrying one food item with 10000 Calories.
+In case the Elves get hungry and need extra snacks, they need to know which Elf to ask: they'd like to know how many Calories are being carried by the Elf carrying the most Calories. In the example above, this is 24000 (carried by the fourth Elf).
+
+Find the Elf carrying the most Calories. How many total Calories is that Elf carrying? */
+
+
+
+function countCalories(input){
+    let itemList = input.split("\n")
+    let highest = 0;
+    let currentSum = 0;
+for(let element of itemList){
+    if (element.trim()){
+        currentSum += Number(element.trim());
+    } else {
+        if (currentSum > highest){
+            highest = currentSum;
+        }
+        currentSum = 0;
+    }
+}
+return highest;
+}
+
+  
+await client.run([countCalories], true);
+
---
+++ README.md
@@ -0,0 +1,9 @@
+❄️🛷🎄🎅🏻☃️🎁❄️🛷🎄🎅🏻☃️🎁❄️🛷🎄🎅🏻☃️🎁
+
+My attempt at the [advent-of-code 2022](https://adventofcode.com/2022/about).
+
+I am using [advent-of-code-client](https://www.npmjs.com/package/advent-of-code-client?activeTab=readme) to fetch the input and test my answers. The token is the session cookie which I stored in an .env file.
+
+Feel free to reach out with any suggestions to improve my code!
+
+❄️🛷🎄🎅🏻☃️🎁❄️🛷🎄🎅🏻☃️🎁❄️🛷🎄🎅🏻☃️🎁

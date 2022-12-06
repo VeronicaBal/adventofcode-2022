226813f87a331008b4d3667c41ea3494027e3e54
Veronica <veronica_baldin@live.it>
2022-12-06 12:34:16 +0100

    Day 3 part 2

--- Day3.js
+++ Day3.js
@@ -11,48 +11,49 @@ const client = new AocClient({
 
 const input = await client.getInput();
 
-/* One Elf has the important job of loading all of the rucksacks with supplies for the jungle journey. Unfortunately, that Elf didn't quite follow the packing instructions, and so a few items now need to be rearranged.
+let rucksacks = input.split(`\n`);
 
-Each rucksack has two large compartments. All items of a given type are meant to go into exactly one of the two compartments. The Elf that did the packing failed to follow this rule for exactly one item type per rucksack.
-
-The Elves have made a list of all of the items currently in each rucksack (your puzzle input), but they need your help finding the errors. Every item type is identified by a single lowercase or uppercase letter (that is, a and A refer to different types of items).
-
-The list of items for each rucksack is given as characters all on a single line. A given rucksack always has the same number of items in each of its two compartments, so the first half of the characters represent items in the first compartment, while the second half of the characters represent items in the second compartment.
-
-For example, suppose you have the following list of contents from six rucksacks:
-
-vJrwpWtwJgWrhcsFMMfFFhFp
-jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
-PmmdzqPrVvPwwTWBwg
-wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
-ttgJtRGJQctTZtZT
-CrZsJsPPZsGzwwsLwLmpwMDw
-The first rucksack contains the items vJrwpWtwJgWrhcsFMMfFFhFp, which means its first compartment contains the items vJrwpWtwJgWr, while the second compartment contains the items hcsFMMfFFhFp. The only item type that appears in both compartments is lowercase p.
-The second rucksack's compartments contain jqHRNqRjqzjGDLGL and rsFMfFZSrLrFZsSL. The only item type that appears in both compartments is uppercase L.
-The third rucksack's compartments contain PmmdzqPrV and vPwwTWBwg; the only common item type is uppercase P.
-The fourth rucksack's compartments only share item type v.
-The fifth rucksack's compartments only share item type t.
-The sixth rucksack's compartments only share item type s.
-To help prioritize item rearrangement, every item type can be converted to a priority:
-
-Lowercase item types a through z have priorities 1 through 26.
-Uppercase item types A through Z have priorities 27 through 52.
-In the above example, the priority of the item type that appears in both compartments of each rucksack is 16 (p), 38 (L), 42 (P), 22 (v), 20 (t), and 19 (s); the sum of these is 157.
-
-Find the item type that appears in both compartments of each rucksack. What is the sum of the priorities of those item types? */
-
+/* Find the only item type (letter) that both compartments of a rucksack (halves of a line)
+have in common. Return the sum of the points of all common type scores */
 function prioritySum (input) {
-    let rucksacks = input.split(`\n`);
     let sum = 0;
     for(let rucksack of rucksacks){
+        /* Divide compartments, convert to array, and assign them to variables */
         let compartment1Items =  rucksack.slice(0, rucksack.length/2).split("");
         let compartment2Items = rucksack.slice(rucksack.length/2, rucksack.length).split("");
+        /* Find the commontype, i.e. the first (and only) character contained in both rucksacks */
         let commonType = compartment1Items.find(i=> compartment2Items.includes(i))
+        /* If the commontype is a lowercase letter, add a point from 1-26, else start counting at 27 */ 
         if (commonType.toLowerCase() === commonType){
             sum += (commonType.charCodeAt(0)-96);
         } else {
             sum += (commonType.toLowerCase().charCodeAt(0)-70);
         }
+    }
+    return sum
+}
+
+/* Find the group common type within each group of 3 elves. 
+Return the sum of all the points of the group common types. */
+function groupPriortySum (input) {
+    let sum = 0;
+
+    /* Loop through the input lines increasing the value by 3 (number of elves per group) */ 
+    for(let i=0; i<rucksacks.length; i+=3){
+
+        /* Define the group as the next 3 elves starting at the current index */
+        let group = rucksacks.slice(i, i+3).map(e => e.split(""));
+
+        /* Find the group common type, which is the first letter of the first elf of the group
+        that is also included in the second and third elves */
+        let commonType = group[0].find(t => group[1].includes(t) && group[2].includes(t));
+
+        /* If the commontype is a lowercase letter, add a point from 1-26, else start counting at 27 */ 
+        if (commonType.toLowerCase() === commonType){
+            sum += (commonType.charCodeAt(0)-96);
+        } else {
+            sum += (commonType.toLowerCase().charCodeAt(0)-70);
+        }        
     }
     return sum
 }

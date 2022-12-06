65c3a5a9ae529230f8b6fef9d3558e4213e240d9
Veronica <veronica_baldin@live.it>
2022-12-06 12:08:45 +0100

    Day 2 part 2

--- Day2.js
+++ Day2.js
@@ -11,45 +11,29 @@ const client = new AocClient({
 
 const input = await client.getInput();
 
-/* The Elves begin to set up camp on the beach. To decide whose tent gets to be closest to the snack storage, a giant Rock Paper Scissors tournament is already in progress.
+/* This object represents the points assigned to each move */ 
+const pointLegend = {rock:1, paper:2, scissors:3};
+/* This object contains key-value pairs of opponent moves correspondance */
+const opponentsLegend = {A: "rock", B: "paper", C: "scissors"};
+/* This object contains the move that will lead to a victory based on the opponent move */
+const winningMoves = {rock:"paper", paper:"scissors", scissors:"rock"}
+/* Get each game (line of the input)*/
+let game = input.split("\n");
+game = game.map(element => element.split(" "));
 
-Rock Paper Scissors is a game between two players. Each game contains many rounds; in each round, the players each simultaneously choose one of Rock, Paper, or Scissors using a hand shape. Then, a winner for that round is selected: Rock defeats Scissors, Scissors defeats Paper, and Paper defeats Rock. If both players choose the same shape, the round instead ends in a draw.
-
-Appreciative of your help yesterday, one Elf gives you an encrypted strategy guide (your puzzle input) that they say will be sure to help you win. "The first column is what your opponent is going to play: A for Rock, B for Paper, and C for Scissors. The second column--" Suddenly, the Elf is called away to help with someone's tent.
-
-The second column, you reason, must be what you should play in response: X for Rock, Y for Paper, and Z for Scissors. Winning every time would be suspicious, so the responses must have been carefully chosen.
-
-The winner of the whole tournament is the player with the highest score. Your total score is the sum of your scores for each round. The score for a single round is the score for the shape you selected (1 for Rock, 2 for Paper, and 3 for Scissors) plus the score for the outcome of the round (0 if you lost, 3 if the round was a draw, and 6 if you won).
-
-Since you can't be sure if the Elf is trying to help you or trick you, you should calculate the score you would get if you were to follow the strategy guide.
-
-For example, suppose you were given the following strategy guide:
-
-A Y
-B X
-C Z
-This strategy guide predicts and recommends the following:
-
-In the first round, your opponent will choose Rock (A), and you should choose Paper (Y). This ends in a win for you with a score of 8 (2 because you chose Paper + 6 because you won).
-In the second round, your opponent will choose Paper (B), and you should choose Rock (X). This ends in a loss for you with a score of 1 (1 + 0).
-The third round is a draw with both players choosing Scissors, giving you a score of 3 + 3 = 6.
-In this example, if you were to follow the strategy guide, you would get a total score of 15 (8 + 1 + 6).
-
-What would your total score be if everything goes exactly according to your strategy guide? */
-
-function rockPaperScissor (input) {
-    let pointLegend = {X:1, Y:2, Z:3};
-    let opponentsLegend = {A: "X", B: "Y", C: "Z"};
-    let winningMoves = {X:"Y", Y:"Z", Z:"X"}
-    let game = input.split("\n");
-    game = game.map(element => element.split(" "));
+function rockPaperScissor(input) {
+    /* This object contains key-value pairs of player moves correspondance */
+    const myMoveLegend = {X:"rock", Y:"paper", Z:"scissors"};
     let points = 0;
 
     for (let round of game){
-        let myMove = round[1];
+        let myMove = myMoveLegend[round[1]];
         let opponentsMove = opponentsLegend[round[0]];
+        /*Add points corresponding to my move*/
         points += pointLegend[myMove];
     
+        /*If my move is the winning move based on the opponents move, add 6 points
+        else, if it is a draw, add 3 points*/
         if(winningMoves[opponentsMove] === myMove){
             points += 6;
         } else if(opponentsMove === myMove){
@@ -59,6 +43,31 @@ function rockPaperScissor (input) {
     return points;
 }
 
+/* In this function we assume that the second column represents the outcome of the round */
+function partTwo(input) {
+ let points = 0;
+ for (let round of game){
+    let outcome = round[1];
+    let opponentsMove = opponentsLegend[round[0]];
+
+    if (outcome === "Y") {
+        /* It's a draw, we add 3 points + same move as opponent */ 
+        points += 3;
+        points += pointLegend[opponentsMove];
+    } else if (outcome === "Z"){
+        /* We win, add 6 points + winning move */
+        points += 6;
+        let winningMove = winningMoves[opponentsMove];
+        points += pointLegend[winningMove];
+    } else if (outcome === "X"){
+        /* We lose, add points of losing move (not same as opponent and not winning) */ 
+        let possibleMoves = Object.keys(pointLegend);
+        let myMove = possibleMoves.filter(m => m !== opponentsMove && m !== winningMoves[opponentsMove]);
+        points += pointLegend[myMove];
+    }
+ }
+ return points;
+}
 
 
  await client.run([rockPaperScissor], true);
